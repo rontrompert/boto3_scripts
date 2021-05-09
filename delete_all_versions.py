75b9b1f40196e8b1a91d24e50665889e194c41d6
Ron Trompert <ron.trompert@surf.nl>
2021-05-09 15:21:10 +0200

    meh

--- delete_all_versions.py
+++ delete_all_versions.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+
+#
+# Copyright [2021] [SURF]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import boto3
 import json
 import sys
@@ -11,11 +27,7 @@ endpoint=''
 region=''
 
 # This should be 1000 or less. The 1000 is an AWS thingie.
-maxkeys=500
-
-# List the stuff that is deleted or not
-Quiet=True
-
+maxkeys=1000
 
 # Choose one from DEBUG, INFO, WARNING, ERROR, CRITICAL
 level=logging.INFO
@@ -94,7 +106,7 @@ while is_truncated:
         logging.warning("Nothing to delete")
         sys.exit(0)
 
-    Delete={'Objects':Objects, 'Quiet': Quiet}
+    Delete={'Objects':Objects, 'Quiet': True}
 
     response=client.delete_objects(Bucket=s3bucket,
                                    Delete=Delete)
